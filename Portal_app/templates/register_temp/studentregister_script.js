25157f2bd38de05e6d28540100ed20c006c8df2d
amritansh-raj <amritansh.raj21@gmail.com>
2023-10-15 22:20:20 +0530

    minor changes

--- Portal_app/templates/register_temp/studentregister_script.js
+++ Portal_app/templates/register_temp/studentregister_script.js
@@ -1,7 +1,9 @@
-myApp.controller("studentregisterController", function ($scope, $http, $state, $location) {
-
+myApp.controller("studentregisterController", [
+  "$scope",
+  "httpService",
+  "$state",
+  function ($scope, httpService, $state) {
     $scope.register = function () {
-
       showLoader();
       pass = $scope.password;
       console.log(pass);
@@ -14,80 +16,66 @@ myApp.controller("studentregisterController", function ($scope, $http, $state, $
         hideLoader();
       }
 
-      var userData = {
-        firstname : $scope.Firstname,
-        lastname : $scope.lastname,
-        username :$scope.username,
-        email :$scope.email,
-       age: $scope.age,
-       address: $scope.address,
-       phone: $scope.phone,
-       fathername: $scope.fathername,
-       mothername: $scope.fathername,
-        pass : pass,
+      var sendData = {
+        firstname: $scope.Firstname,
+        lastname: $scope.lastname,
+        username: $scope.username,
+        email: $scope.email,
+        age: $scope.age,
+        address: $scope.address,
+        phone: $scope.phone,
+        fathername: $scope.fathername,
+        mothername: $scope.fathername,
+        pass: pass,
         id: $scope.selectedDepartment,
-        
       };
-  
-      var sendData = JSON.stringify(userData);
+
       console.log(sendData);
-  
-  
-  
-      $http
-        .post("https://10.21.81.251:8000/a", sendData, {
-          headers: { 'Content-Type': undefined },
-          withCredentials: true
-        })
-  
-        .then(function (sendData) {
-          var register = sendData.data;
+
+      httpService
+        .post("", sendData)
+        .then((response) => {
+          var register = response.data;
           console.log(register);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          hideLoader();
         });
-      $state.go('login');
     };
-  
+
     $scope.coursess = [];
     $scope.departments = [];
     $scope.selectedcourses = null;
-  
-  
+
     $scope.loadDoctors = function () {
       if ($scope.selectedcourses) {
-        $http.get('https://10.21.81.251:8000/c', {
-          params: { coursesid: $scope.selectedcourses },
-  
-          headers: { 'Content-Type': undefined },
-          withCredentials: true
-        })
-          .then(function (response) {
+        httpService
+          .get(" ", {
+            params: { coursesid: $scope.selectedcourses },
+          })
+          .then((response) => {
             console.log(response);
             $scope.departments = response.data;
-  
           })
-          .catch(function (error) {
+          .catch((error) => {
             console.log(error);
           });
       } else {
-  
         $scope.departments = [];
       }
     };
-  
-  
-    $http.get('https://10.21.81.251:8000/a', {
-      headers: { 'Content-Type': undefined },
-      withCredentials: true
-    })
-  
-      .then(function (response) {
+
+    httpService
+      .get(" ")
+      .then((response) => {
         console.log(response);
         $scope.coursess = response.data;
       })
-      .catch(function (error) {
+      .catch((error) => {
         console.log(error);
       });
-  
-  
-  
-  });
+  },
+]);
