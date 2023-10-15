e6563d9895f805bbcd53b46e6e874dca3cfdd03d
amritansh-raj <amritansh.raj21@gmail.com>
2023-10-15 15:48:07 +0530

    logout

--- Portal_app/templates/login_temp/login_script.js
+++ Portal_app/templates/login_temp/login_script.js
@@ -1,9 +1,8 @@
 myApp.controller("loginController", [
   "$scope",
-  "$http",
   "$state",
   "httpService",
-  function ($scope, $http, $state, httpService) {
+  function ($scope, $state, httpService) {
     $scope.login = function () {
       showLoader();
       username = $scope.username;
@@ -11,49 +10,34 @@ myApp.controller("loginController", [
       console.log(username);
       console.log(pass);
 
-      if (!validatePass(pass)) {
-        $scope.loginForm.password.$setValidity("password", false);
-        hideLoader();
-      } else {
-        $scope.loginForm.password.$setValidity("password", true);
-        hideLoader();
-      }
-
       postData = {
         username: username,
         password: pass,
       };
 
-      httpService
-        .post("login_user/", postData)
-        .then(function (response) {
-          console.log(response);
-          role = response.data[0].role;
-          console.log(role);
-          if(role === 1){
-            $state.go("dashBoard");
-          }
-          alertify.success("Ok");
-        })
-        .catch(function (error) {
-          console.error(error);
-        })
-        .finally(function () {
-          hideLoader();
-        });
-
-      // $http({
-      //   method : "POST",
-      //   url : "https://10.21.85.67:8000/demoadmin/login_user/",
-      //   data : postData,
-      //   withCredentials : true
-      // })
-      //   .then(function(response){
-      //     console.log(response)
-      //   })
-      //   .catch(function(error){
-      //     console.log(error)
-      //   })
+      if (!validatePass(pass)) {
+        $scope.loginForm.password.$setValidity("password", false);
+        hideLoader();
+      } else {
+        $scope.loginForm.password.$setValidity("password", true);
+        httpService
+          .post("login/", postData)
+          .then((response) => {
+            console.log(response);
+            role = response.data[0].role;
+            console.log(role);
+            if (role === 1) {
+              $state.go("dashBoard");
+            }
+            alertify.success("logged in succesfully!!");
+          })
+          .catch((error) => {
+            console.error(error);
+          })
+          .finally(() => {
+            hideLoader();
+          });
+      }
     };
   },
 ]);
