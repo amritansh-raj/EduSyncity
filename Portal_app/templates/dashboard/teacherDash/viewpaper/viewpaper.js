ffec1ab9cf20871eee73b10dcd7a6b34d1a295c5
amritansh-raj <amritansh.raj21@gmail.com>
2023-10-30 18:20:04 +0530

    viewpaper update

--- Portal_app/templates/dashboard/teacherDash/viewpaper/viewpaper.js
+++ Portal_app/templates/dashboard/teacherDash/viewpaper/viewpaper.js
@@ -1,5 +1,20 @@
 myApp.controller("viewPaperController", [
   "$scope",
   "httpService",
-  function ($scope, httpService) {},
+  function ($scope, httpService) {
+    httpService
+      .get("eduexam/view_paper/")
+      .then((r) => {
+        qPaper = r.data;
+
+        if (qPaper) {
+          $scope.qPaper = qPaper;
+        }
+
+        console.log($scope.qPaper);
+      })
+      .catch((e) => {
+        console.log(e.data);
+      });
+  },
 ]);
